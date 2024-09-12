a98befb6bafb2aa487ad3ebd1794c49b1fc650ae
elida <elidakirigo@gmail.com>
2024-09-12 22:49:41 +0300

    dependencies - add husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,29 @@
-import { Button } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
+import Link from 'next/link'
 
 export default function Home() {
 	return (
-		<div>
-			<div className=''>
-				<Button
-					variant='outline'
-					size='sm'
-					// onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}
-				>
-					Login
-				</Button>
-				<Button
-					variant='outline'
-					size='sm'
-					// onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}
-				>
-					SignUp
-				</Button>
-			</div>
+		<div className=''>
+			<section className=''>
+				<header className='flex items-center justify-center pt-5'>
+				
+
+					<Button
+						variant='ghost'
+						size='sm'
+						// onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}
+					>
+						Login
+					</Button>
+					<Button
+						variant='outline'
+						size='sm'
+						// onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}
+					>
+						SignUp
+					</Button>
+				</header>
+			</section>
 		</div>
 	)
 }
