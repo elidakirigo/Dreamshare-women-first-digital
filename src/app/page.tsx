6b77e2bfa33c5a1ad2ab538363ae478f26b1500e
elida <elidakirigo@gmail.com>
2024-09-12 21:42:54 +0300

    core - add custom hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
-	return <div></div>
+	return (
+		<div>
+			<div className=''>
+				<Button
+					variant='outline'
+					size='sm'
+					// onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}
+				>
+					Login
+				</Button>
+				<Button
+					variant='outline'
+					size='sm'
+					// onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}
+				>
+					SignUp
+				</Button>
+			</div>
+		</div>
+	)
 }
