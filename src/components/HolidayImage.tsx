eae365ebb2e4ae9c17a2e24b0da7b23905c7c963
elida <elidakirigo@gmail.com>
2024-09-17 00:34:59 +0300

    feature(images) - add holiday section

--- src/components/HolidayImage.tsx
+++ src/components/HolidayImage.tsx
@@ -13,9 +13,8 @@ const HolidayImages = ({ ImageUrl, title }: ImageProps) => {
 				<Image src={ImageUrl} alt='' className='min-h-80 rounded-md object-cover shadow-lg' fill={true} />
 			</AspectRatio>
 			<div className='absolute left-0 top-0 flex h-full w-full flex-col items-start justify-end gap-10 rounded bg-zinc-900/20 p-6 text-white opacity-100 duration-500'>
-				<span className='rounded-3xl bg-[#FF4E50] px-4 py-1 font-light'>STEP {step}</span>
+			
 				<h1 className='text-3xl font-bold'>{title}</h1>
-				<p>{description}</p>
 			</div>
 		</div>
 	)
