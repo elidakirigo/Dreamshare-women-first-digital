51896f4db5419fd8eeedc1bf9f51a74a1ca19486
elida <elidakirigo@gmail.com>
2024-09-19 18:21:25 +0300

    fix(SEO)page speed performance optimization on the movies section - add next configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,19 @@ const nextConfig = {
 	images: {
 		// unoptimized: true,
 		// domains: ['image.tmdb.org', 'randomuser.me'],
-		remotePatterns: ['image.tmdb.org', 'randomuser.me'],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'image.tmdb.org',
+				pathname: '**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'randomuser.me',
+				pathname: '**',
+			},
+		],
 	},
 }
 
-export default nextConfig;
+export default nextConfig
