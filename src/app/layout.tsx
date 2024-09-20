1cd238de92b8b6023b4882292ed2b82d1dfb8eb2
elida <elidakirigo@gmail.com>
2024-09-20 08:39:49 +0300

    feature(google analytics) - add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { SessionProvider } from '@/components/SessionProvider'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/authoptions'
 
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 
 const montserrat = Montserrat({ weight: ['100', '500'], subsets: ['latin'] })
 
@@ -26,6 +26,7 @@ export default async function RootLayout({
 			<link rel='icon' href='/favicon.svg' sizes='any' />
 			<body className={montserrat.className}>
 				<SessionProvider session={session}>
+					<GoogleTagManager gtmId='G-E6QC2G1KE3' />
 					{children}
 					<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string} />
 				</SessionProvider>
